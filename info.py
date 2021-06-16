49d391042453519e462d416edb51d3de6de32105
Abigail G <dev@kb6.ee>
2021-06-16 00:12:51 -0400

    fix issue with reddit feed

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 """
 Static info about the bot.
 ---
-Copyright (C) 2019-2021 classabbyamp
+Copyright (C) 2021 classabbyamp
 
 This file is part of r-wlw-bot and is released under the terms of
 the GNU General Public License, version 2.
@@ -12,5 +12,5 @@ authors = ("@ClassAbbyAmplifier#2229",)
 description = """A bot with various functions for the r/wlw discord server, written in Python."""
 license = "Released under the GNU General Public License v2"
 contributing = "Check out the [source on GitHub](https://github.com/classabbyamp/r-wlw-bot). Contributions are welcome!"
-release = "1.0.0"
+release = "1.0.1"
 bot_server = "https://discord.gg/YrJWRrND7T"
